6e40b800131d51f51f5b5076200096cee734b173
Patrik Beck <patrik.beck@gmail.com>
2017-04-02 11:18:38 +0200

    plotting implemented

--- drawdowns.py
+++ drawdowns.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from scipy.signal import argrelextrema
 import matplotlib.pyplot as plt
+import matplotlib
+
 plt.style.use('ggplot')
 
 from collections import namedtuple
@@ -12,9 +14,11 @@ import sys
 
 Drawdown = namedtuple('Drawdown', 'start end percentage')
 
+
 def get_dataframe(file_name):
     df = pd.read_csv(file_name, index_col='Date', parse_dates=True)
     return df.reindex(index=df.index[::-1])
+
 
 def find_drawdowns(df, percentage_limit):
     current_max_idx = 0
@@ -31,7 +35,7 @@ def find_drawdowns(df, percentage_limit):
 
                 drawdown_percentage = 100 - 100*price/current_max_price
 
-                if drawdown_percentage >= drawdown_limit:
+                if drawdown_percentage >= percentage_limit:
 
                     drawdowns.append(Drawdown(current_max_idx, idx - 1, drawdown_percentage))
 
@@ -42,12 +46,24 @@ def find_drawdowns(df, percentage_limit):
 
     return drawdowns
 
-def plot(df):
-    fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(15,8))
-    ax[0].plot(df.index, df.Close)
-    ax[1].bar(df.index, df.Volume, width=1/(5*len(df.index)))
+
+def plot_drawdown(df, drawdown):
+    data = df[max(drawdown.start - 4, 0):min(drawdown.end + 4, len(df.index))]
+
+    fig = plt.figure(figsize=(15, 9))
+
+    plt.axvspan(df.index[drawdown.start], df.index[drawdown.end], color='red', alpha=0.3)
+
+    ax = fig.add_subplot(1,1,1)
+    ax.set_position(matplotlib.transforms.Bbox([[0.125,0.1],[0.9,0.9]]))
+    ax.plot(data.index, data.Close, color='blue')
+
+    ax2 = ax.twinx()
+    ax2.set_position(matplotlib.transforms.Bbox([[0.125,0.1],[0.9,0.32]]))
+    ax2.bar(data.index, data.Volume, 0.3, color='red')
 
     plt.show()
+
 
 if __name__ == "__main__":
     #the script was executed directly
@@ -64,5 +80,6 @@ if __name__ == "__main__":
 
     for d in drawdowns:
         print d
-
-    plot(df[0:20])
+        
+    for d in drawdowns:
+        plot_drawdown(df, d)
